d3a73f52a579d18a90155800bd2a65a1603c99cb
icqun <jln.schludermann@gmail.com>
2024-03-05 11:55:07 +0100

    fix: docker login ghcr

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 mvn -B package
 cp src/main/docker/Dockerfile target/
-docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
+# docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
 docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
 docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
